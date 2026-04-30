caee3945be0a6d3c9342c47f9be8b9dc73df3915
Niroj Tamang <niroj6988@gmail.com>
2026-04-30 16:58:44 +0545

    Footer is added in ForBusinessPage

--- src/Pages/ForBusinessPage.tsx
+++ src/Pages/ForBusinessPage.tsx
@@ -1,11 +1,17 @@
+import Footer from "../Components/Footer"
 import Content from "../Features/ForBusiness/Componeents/Content"
+
 
 function ForBusinessPage () {
 
 
     return(
 <>
+
+        <div className="bg-[#0B0B0C]">
         <Content/>
+        <Footer/>
+        </div>
 
 </>
 
